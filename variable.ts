fe9bd50b41f8afa4e2f45919a0cc60fed03a6888
tewei <113736691+thhryuxztnt13546@users.noreply.github.com>
2022-09-28 20:08:44 +0800

    Create variable.ts

---
+++ variable.ts
@@ -0,0 +1,8 @@
+var var1:number = 1;
+var var2:string = 'Hello, World';
+var var3:boolean = true;
+var var4:Array<number> = new Array<number>(1, 2) // or [1, 2]
+var var5:[string, number] = ['bob', 21]
+var var6:never = null // or undefined
+
+var varAny:any = 'There can be anything';
